07bc5de85a527a9fc64eb16a9e52dfe6d909e750
TheRiseOfDavid <david53133@gmail.com>
2024-12-28 21:23:24 +0800

    --amend

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from src.apartment.apartment_happy_crawler import ApartmentHappy
 from src.job.job_1111_crawler import job_1111_crawler
 
 # Apartment591Carwler = Apartment591Carwler()
-# Apartment591Carwler.crawler_591()
-# ApartmentHappy = ApartmentHappy()
-# data = ApartmentHappy.get_web_url() 
-job_1111_crawler = job_1111_crawler() # TODO: 網頁資料有改
-job_1111_crawler.job_link()
+# Apartment591Carwler.crawler_591()  # 591 不讓爬
+ApartmentHappy = ApartmentHappy()
+data = ApartmentHappy.get_web_url() 
+# job_1111_crawler = job_1111_crawler() 
+# job_1111_crawler.job_link()
 #print(job_1111_crawler.get_web_data("https://www.1111.com.tw/job/91416768/"))
